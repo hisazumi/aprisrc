8ef45395a5efc29b1a6da8b6580b6037372783fc
Kenji Hisazumi <hisazumi@gmail.com>
2018-10-02 22:22:00 +0900

    'refactored'

--- key_and_ArUco.py
+++ key_and_ArUco.py
@@ -27,15 +27,48 @@ def flight_data_handler(event, sender, data, **args):
 
 
 skip_frames = 300
+drone = None
 container = None
 dictionary_name = cv2.aruco.DICT_4X4_50 
 dictionary = cv2.aruco.getPredefinedDictionary(dictionary_name)
+
+def on_press(key):
+    key_code = format(key.char)
+    print('{0} pressed'.format(key_code))
+    if key_code == 't':
+        print("Start Take-Off!")
+        drone.takeoff()
+        time.sleep(5)
+    elif key_code == 'l':
+        print("Start Landing!")
+        drone.down(50)
+        time.sleep(5)
+        drone.land()
+    elif key_code == "Key.left":
+        print("left")
+        drone.left(20)
+    elif key_code == "Key.right":
+        print("right")
+        drone.right(20)
+    elif key_code == "Key.up":
+        print("forward")
+        drone.forward(20)
+        time.sleep(1)
+    elif key_code == "Key.down":
+        drone.backward(20)
+
+def on_release(key):
+    key_code = format(key.char)
+    print('{0} released'.format(key_code))
+    drone.forward(0)
+    drone.backward(0)
+    drone.right(0)
+    drone.left(0)
 
 def video_handler():
     global container
     global skip_frames
 
-    # skip skip_frames frames
     try:
         for frame in container.decode(video=0):
             if 0 < skip_frames:
@@ -57,45 +90,8 @@ def video_handler():
         traceback.print_exception(exc_type, exc_value, exc_traceback)
         print(ex)
 
-
-def update(old, new, max_delta=0.3):
-    if abs(old - new) <= max_delta:
-        res = new
-    else:
-        res = 0.0
-    return res
-
-
-
-key_code=0
-key_condition=0
-key_timing = 0
-
-
-def on_press(key):
-    global key_code, key_condition, key_timing
-    try:
-        #print('alphanumeric key {0} pressed'.format(key.char))
-        key_code = format(key.char)
-    except AttributeError:
-        #print('special key {0} pressed'.format(key))
-        key_code = format(key)
-    key_condition = 1
-    key_timing = 1
-
-
-def on_release(key):
-    global key_code, key_condition, key_timing
-    #print('{0} released'.format(key))
-    key_condition = 0
-    key_code = format(key)
-    key_timing = 1
-
-
 def main():
-    global key_code, key_condition,key_timing
-    condition = 0;
-    direct = 0
+    global container, drone
 
     drone = tellopy.Tello()
     drone.video_encoder_rate = 1
@@ -103,73 +99,15 @@ def main():
     drone.wait_for_connection(60.0)
     container = av.open(drone.get_video_stream())
     drone.subscribe(drone.EVENT_FLIGHT_DATA, flight_data_handler)
-    #drone.subscribe(drone.EVENT_VIDEO_FRAME, video_handler)
-    speed = 100
-    throttle = 0.0
-    yaw = 0.0
-    pitch = 0.0
-    roll = 0.0
 
-    listener = Listener(on_press = on_press,on_release= on_release)
+    listener = Listener(on_press = on_press, on_release= on_release)
     listener.start()
 
-
     try:
-        while 1:
+        while True:
             # loop with pygame.event.get() is too much tight w/o some sleep
             video_handler()
             time.sleep(0.01)
-
-            if key_timing  == 1: # key change 
-                key_timing = 0;
-                if key_condition == 1: #key_condition = 1 press
-                    print('alphanumeric key {0} pressed'.format(key_code))
-                    if key_code == 't'and condition == 0:
-                        print("Start Take-Off!")
-                        drone.takeoff()
-                        sleep(5)
-                        condition = 1
-                    elif key_code == 'l'and condition == 1:
-                        print("Start Landing!")
-                        drone.down(50)
-                        sleep(5)
-                        drone.land()
-                        condition = 0
-                    elif key_code == "Key.left":
-                        print("left")
-                        drone.left(20)
-                        direct=4
-                    elif key_code == "Key.right":
-                        print("right")
-                        drone.right(20)
-                        direct=3
-                    elif key_code == "Key.up":
-                        print("forward")
-                        drone.forward(20)
-                        sleep(1)
-                        direct=1
-                    elif key_code == "Key.down":
-                        drone.backward(20)
-                        direct=2
-                    elif key_code == "Key.esc":
-                        if condition == 1:
-                            print("Start Landing!")
-                            drone.down(50)
-                            sleep(5)
-                            drone.land()
-                            condition = 0
-                        break
-                else: #key_condition=0 release
-                    print('{0} released'.format(key_code))
-                    if direct == 1:
-                        drone.forward(0)
-                    elif direct == 2:
-                        drone.backward(0)
-                    elif direct == 3:
-                        drone.right(0)
-                    elif direct == 4:
-                        drone.left(0)
-                    direct=0
 
     except KeyboardInterrupt as e:
         print(e)
